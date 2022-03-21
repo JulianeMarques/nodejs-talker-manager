98094868d01c649e0bbf9d430af97eb7d1ce284a
Juliane Marques <mqs.juliane@gmail.com>
2022-03-21 11:40:27 -0300

    fix: lint

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
-const { application, response } = require('express');
+
 const TALKER = 'talker.json';
 
 const app = express();
