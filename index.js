91a237f62c7a75d355ae731437b49a63240c503a
Juliane Marques <mqs.juliane@gmail.com>
2022-03-23 18:52:38 -0300

    try: trying

--- index.js
+++ index.js
@@ -41,3 +41,39 @@ app.get('/talker/:id', (req, res) => {
   }
   return res.status(200).json(person);
 });
+
+// 3. O endpoint deve ser capaz de retornar um token aleatório de 16 caracteres que deverá ser utilizado nas demais requisições.
+app.post('./login', (req, res) => {
+
+  return res.status(200).json({ token });
+});
+
+// token: https://www.codegrepper.com/code-examples/javascript/create+16+char+token+jsv
+// var crypto = require("crypto");
+// var id = crypto.randomBytes(8).toString('hex');
+
+const crypto = require("crypto");
+const createToken = () => {
+  return crypto.randomBytes(8).toString('hex');
+};
+
+const validEmail = (req, res, next) => {
+  const { email } = req.body;
+  if (!email || email === '') {
+    return res.status(400).json({
+      message: 'O campo "email" é obrigatório',
+    });
+  };
+};
+
+const validPassword = (req, res, next) => {
+  const { password } = req.body;
+  if (!password) {
+    return 'O campo "password" é obrigatório';
+  }
+  if (password.length < 6) {
+    return 'O "password" deve ter pelo menos 6 caracteres';
+    return undefined;
+  }
+};
+
